add2394e08bb2649fc4c0141babd7e15cef959e5
JulianCambraia <juliancambraia@gmail.com>
2022-11-20 18:44:55 -0300

    Aplica a política de Auto Scaling com Modo de Cobrança On-Demand (Paga por Requests) e remove adequação na capacidade de Leitura e Escrita parâmetros setados na Stack do DinamoDBStack.

--- curso_aws_cdk/src/main/java/com/myorg/DynamoDBStack.java
+++ curso_aws_cdk/src/main/java/com/myorg/DynamoDBStack.java
@@ -1,12 +1,15 @@
 package com.myorg;
 
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.dynamodb.Attribute;
 import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.BillingMode;
+import software.amazon.awscdk.services.dynamodb.EnableScalingProps;
 import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.UtilizationScalingProps;
 import software.constructs.Construct;
 
 public class DynamoDBStack extends Stack {
@@ -23,7 +26,9 @@ public class DynamoDBStack extends Stack {
         // Criação da Estrutura Básica da Tabela a ser criada no DynamoDB
         productEventsDdb = Table.Builder.create(this, "ProductEventsDdb")
                 .tableName("product-events")
-                .billingMode(BillingMode.PAY_PER_REQUEST) // Tipo de Cobrança por acesso a tabela no DynamoDB
+                .billingMode(BillingMode.PROVISIONED) // Tipo de Cobrança por acesso a tabela no DynamoDB
+                .readCapacity(1) // capacidade de leitura e escrita por segundo na Tabela do DynamoDB
+                .writeCapacity(1)
                 .partitionKey(Attribute.builder()
                         .name("pk")
                         .type(AttributeType.STRING)
@@ -35,6 +40,30 @@ public class DynamoDBStack extends Stack {
                 .timeToLiveAttribute("ttl") // tempo
                 .removalPolicy(RemovalPolicy.DESTROY) // Ao apagar a Stack devera destruir a Tabela criada no DynamoDB
                 .build();
+
+        // Politica de Scaling Provisionade
+        // Configurando Scaling para adequar a capacidade de requisições esperadas.
+        // Leitura
+        productEventsDdb.autoScaleReadCapacity(EnableScalingProps.builder()
+                        .minCapacity(1)
+                        .maxCapacity(4)
+                        .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
+        // Escrita
+        productEventsDdb.autoScaleWriteCapacity(EnableScalingProps.builder()
+                        .minCapacity(1)
+                        .maxCapacity(4)
+                        .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
+
     }
 
     public Table getProductEventsDdb() {
