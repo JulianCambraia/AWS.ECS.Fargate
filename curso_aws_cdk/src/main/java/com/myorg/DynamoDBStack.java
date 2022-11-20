f4d312d3dd3f96aab791ce8157bb8544cf01014a
JulianCambraia <juliancambraia@gmail.com>
2022-11-20 18:11:47 -0300

    Aplica a política de Auto Scaling com Modo de Cobrança Provisionado e adequando a capacidade de Leitura e Escrita para se auto ajustar conforme parâmetros setados na Stack do DinamoDBStack.

--- curso_aws_cdk/src/main/java/com/myorg/DynamoDBStack.java
+++ curso_aws_cdk/src/main/java/com/myorg/DynamoDBStack.java
@@ -1,12 +1,15 @@
 package com.myorg;
 
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.dynamodb.Attribute;
 import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.BillingMode;
+import software.amazon.awscdk.services.dynamodb.EnableScalingProps;
 import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.UtilizationScalingProps;
 import software.constructs.Construct;
 
 public class DynamoDBStack extends Stack {
@@ -37,6 +40,29 @@ public class DynamoDBStack extends Stack {
                 .timeToLiveAttribute("ttl") // tempo
                 .removalPolicy(RemovalPolicy.DESTROY) // Ao apagar a Stack devera destruir a Tabela criada no DynamoDB
                 .build();
+
+        // Politica de Scaling Provisionade
+        // Configurando Scaling para adequar a capacidade de requisições esperadas.
+        // Leitura
+        productEventsDdb.autoScaleReadCapacity(EnableScalingProps.builder()
+                        .minCapacity(1)
+                        .maxCapacity(4)
+                        .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
+        // Escrita
+        productEventsDdb.autoScaleWriteCapacity(EnableScalingProps.builder()
+                        .minCapacity(1)
+                        .maxCapacity(4)
+                        .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
 
     }
 
