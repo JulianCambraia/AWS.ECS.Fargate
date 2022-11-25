5e86a336edba5f525223abbd9cc2c6c8f4824d66
JulianCambraia <juliancambraia@gmail.com>
2022-11-25 19:14:49 -0300

    Adiciona as classes de configuração para o S3, SQS Cria properties para acesso do bucket S3 e ao SQS

---
+++ aws_project01/src/main/java/br/com/juliancambraia/aws_project01/config/JmsConfig.java
@@ -0,0 +1,50 @@
+package br.com.juliancambraia.aws_project01.config;
+
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.destination.DynamicDestinationResolver;
+
+import javax.jms.Session;
+
+@Configuration
+@EnableJms
+@Profile("!local")
+public class JmsConfig {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    SQSConnectionFactory sqsConnectionFactory;
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        sqsConnectionFactory = new SQSConnectionFactory(
+                new ProviderConfiguration(),
+                AmazonSQSClientBuilder.standard()
+                        .withRegion(awsRegion)
+                        .withCredentials(new DefaultAWSCredentialsProviderChain())
+                        .build());
+
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(sqsConnectionFactory);
+        factory.setDestinationResolver(new DynamicDestinationResolver());
+        factory.setConcurrency("2");
+        factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+
+        return factory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        return new JmsTemplate(sqsConnectionFactory);
+    }
+}
---
+++ aws_project01/src/main/java/br/com/juliancambraia/aws_project01/config/S3Config.java
@@ -0,0 +1,24 @@
+package br.com.juliancambraia.aws_project01.config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("!local")
+public class S3Config {
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public AmazonS3 snsClient() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(awsRegion)
+                .withCredentials(new DefaultAWSCredentialsProviderChain()) // procura credenciais no contexto da aplicação
+                .build();
+    }
+}
